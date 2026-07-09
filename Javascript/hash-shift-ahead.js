5cf85e1ed109efbc06dd252a534877cec7d252fe
Elhaam Basheer Chaudhry <e4elhaam@gmail.com>
2026-07-10 02:18:50 +0500

    feat: todo comment

--- Javascript/hash-shift-ahead.js
+++ Javascript/hash-shift-ahead.js
@@ -56,6 +56,7 @@ var CaesarCipher = function (shift) {
     return encodedChar;
   };
 
+  // TODO: refactor this
   this.getLoopedBackCharAscii = function (asciShift, lowerOrUpper) {
     let loopedBackShift = 0;
     if (lowerOrUpper === "upper") {
